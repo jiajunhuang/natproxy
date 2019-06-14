7513c403b967180b0b9922f785e7e2a74946f5e8
Jiajun Huang <gansteed@gmail.com>
2019-06-14 11:07:34 +0800

    client use chinses

--- client/client.go
+++ client/client.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	version = "0.0.2"
+	version = "0.0.3"
 	arch    = runtime.GOARCH
 	os      = runtime.GOOS
 )
@@ -33,13 +33,13 @@ var (
 func connectServer(addr string) {
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		logger.Error("failed to dial with server", zap.String("addr", addr), zap.Error(err))
+		logger.Error("无法连接服务器", zap.String("服务器地址", addr), zap.Error(err))
 		return
 	}
 
 	localConn, err := net.Dial("tcp", *localAddr)
 	if err != nil {
-		logger.Error("failed to dial with local address", zap.String("addr", *localAddr), zap.Error(err))
+		logger.Error("无法连接本地目标地址", zap.String("本地地址", *localAddr), zap.Error(err))
 		return
 	}
 
@@ -52,46 +52,46 @@ func waitMsgFromServer(addr string) error {
 
 	client, conn, err := dial.WithServer(ctx, *serverAddr, *useTLS)
 	if err != nil {
-		logger.Error("failed to connect to server server", zap.Error(err))
+		logger.Error("无法连接服务器", zap.Error(err))
 		return err
 	}
 	defer conn.Close()
 
-	logger.Info("try to connect to server", zap.String("addr", *serverAddr))
+	logger.Info("准备连接到服务器", zap.String("服务器地址", *serverAddr))
 
 	stream, err := client.Msg(ctx)
 	if err != nil {
-		logger.Error("failed to communicate with server", zap.Error(err))
+		logger.Error("无法与服务器通信", zap.Error(err))
 		return err
 	}
-	logger.Info("success to connect to server", zap.String("addr", *serverAddr))
+	logger.Info("成功连接到服务器", zap.String("服务器地址", *serverAddr))
 
 	// report client version info
 	data, err := proto.Marshal(&pb.ClientInfo{Os: os, Arch: arch, Version: version})
 	if err != nil {
-		logger.Error("failed to marshal message", zap.Error(err))
+		logger.Error("无法压缩信息", zap.Error(err))
 		return err
 	}
 	if err := stream.Send(&pb.MsgRequest{Type: pb.MsgType_Report, Data: data}); err != nil {
-		logger.Error("failed to talk with server", zap.Error(err))
+		logger.Error("无法发送消息到服务器", zap.Error(err))
 		return err
 	}
 
 	for {
 		resp, err := stream.Recv()
 		if err != nil {
-			logger.Error("failed to receive message from server", zap.Error(err))
+			logger.Error("无法从服务器接收消息", zap.Error(err))
 			return err
 		}
 
 		switch resp.Type {
 		case pb.MsgType_Connect:
-			logger.Info("server told me to spawn a new connection", zap.ByteString("addr", resp.Data))
+			logger.Info("服务器要求发起新连接", zap.ByteString("目标地址", resp.Data))
 			go connectServer(string(resp.Data))
 		case pb.MsgType_WANAddr:
-			logger.Info("server told me about WAN listener address", zap.ByteString("WAN listener address", resp.Data))
+			logger.Info("服务器分配的公网地址是", zap.ByteString("公网地址", resp.Data))
 		default:
-			logger.Error("message that current client don't support", zap.Any("msg", resp))
+			logger.Error("当前版本客户端不支持本消息，请升级", zap.Any("消息", resp))
 		}
 	}
 }
